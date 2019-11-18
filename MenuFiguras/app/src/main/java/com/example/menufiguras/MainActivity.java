7b8c4d19b0e0dd3e65beec3ed418a6d19e8896bc
2DAMAarpei <aarpei@alu.iesserpis.org>
2019-11-18 13:50:27 +0100

    Actualizado proyecto figuras

--- MenuFiguras/app/src/main/java/com/example/menufiguras/MainActivity.java
+++ MenuFiguras/app/src/main/java/com/example/menufiguras/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.menufiguras;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
 import android.view.ContextMenu;
@@ -22,12 +24,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         registerForContextMenu(findViewById(R.id.helpButton));
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_principal, menu);
-
-
         return true;
     }
     @Override
@@ -37,16 +38,97 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case  R.id.MnuOpc1Calcula:
                 getLayoutInflater().inflate(R.layout.objects_area_rectangle,viewGroup);
-
+                Button area_rectangle_calculate=findViewById(R.id.edB1R);
+                area_rectangle_calculate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText edT1=findViewById(R.id.edT1R);
+                        EditText edT2=findViewById(R.id.edT2R);
+                        TextView resultado=findViewById(R.id.etV1R_result);
+                        resultado.setText(String.valueOf(Float.parseFloat(edT1.getText().toString())*Float.parseFloat(edT2.getText().toString())));
+                    }
+                });
                 break;
             case  R.id.MnuOpc2Calcula:
                 getLayoutInflater().inflate(R.layout.objects_area_circle,viewGroup);
+                Button area_circle_calculate=findViewById(R.id.edB1C);
+
+                area_circle_calculate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText edT1=findViewById(R.id.edT1C);
+                        TextView resultado=findViewById(R.id.etV1C_result);
+                        resultado.setText(String.valueOf(Math.PI*Math.pow(Float.parseFloat(edT1.getText().toString()),2 )));
+                    }
+                });
                 break;
             case  R.id.MnuOpc3Calcula:
                 getLayoutInflater().inflate(R.layout.objects_area_triangle,viewGroup);
+                Button area_triangle_calculate=findViewById(R.id.edB1T);
+                area_triangle_calculate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText edT1=findViewById(R.id.edT1T);
+                        EditText edT2=findViewById(R.id.edT2T);
+                        TextView resultado=findViewById(R.id.etV1T_result);
+                        resultado.setText(String.valueOf((Float.parseFloat(edT1.getText().toString())*Float.parseFloat(edT2.getText().toString()))/2));
+                    }
+                });
                 break;
             case  R.id.MnuOpc4Calcula:
                 getLayoutInflater().inflate(R.layout.objects_area_square,viewGroup);
+                Button area_square_calculate=findViewById(R.id.edB1T);
+                area_square_calculate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText edT1=findViewById(R.id.edT1S);
+                        TextView resultado=findViewById(R.id.etV1S_result);
+                        resultado.setText(String.valueOf(Math.pow(Float.parseFloat(edT1.getText().toString()),2)));
+                    }
+                });
+                break;
+            case  R.id.MnuOpc1Dibuja:
+                getLayoutInflater().inflate(R.layout.objects_area_square,viewGroup);
+                Button area_rect_calculate=findViewById(R.id.edB1T);
+                area_square_calculate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText edT1=findViewById(R.id.edT1S);
+                        TextView resultado=findViewById(R.id.etV1S_result);
+                        resultado.setText(String.valueOf(Math.pow(Float.parseFloat(edT1.getText().toString()),2)));
+                    }
+                });
+                break;
+            case  R.id.MnuOpc2Dibuja:
+                getLayoutInflater().inflate(R.layout.draw_bitmap,viewGroup);
+                BitmapDrawable draw;
+                draw.
+                logo=(BitmapDrawable)res.getDrawable(R.drawable.pizzadepepperoni2);
+                logo.setBounds(new Rect(30,50,200,200));
+                break;
+            case  R.id.MnuOpc3Dibuja:
+                getLayoutInflater().inflate(R.layout.objects_area_square,viewGroup);
+                Button area_square_calculate=findViewById(R.id.edB1T);
+                area_square_calculate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText edT1=findViewById(R.id.edT1S);
+                        TextView resultado=findViewById(R.id.etV1S_result);
+                        resultado.setText(String.valueOf(Math.pow(Float.parseFloat(edT1.getText().toString()),2)));
+                    }
+                });
+                break;
+            case  R.id.MnuOpc4Dibuja:
+                getLayoutInflater().inflate(R.layout.objects_area_square,viewGroup);
+                Button area_square_calculate=findViewById(R.id.edB1T);
+                area_square_calculate.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        EditText edT1=findViewById(R.id.edT1S);
+                        TextView resultado=findViewById(R.id.etV1S_result);
+                        resultado.setText(String.valueOf(Math.pow(Float.parseFloat(edT1.getText().toString()),2)));
+                    }
+                });
                 break;
             default:
                 break;
@@ -57,9 +139,7 @@ public class MainActivity extends AppCompatActivity {
    @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
     {
-
         super.onCreateContextMenu(menu, v, menuInfo);
-
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_contextual, menu);
     }
@@ -75,8 +155,8 @@ public class MainActivity extends AppCompatActivity {
 
             default:
                 return super.onContextItemSelected(item);
-
         }
     }
 
 }
+
