0f695a6821ec3c291f3812f045ea04852358c0ca
2DAMAarpei <aarpei@alu.iesserpis.org>
2019-11-19 13:59:44 +0100

    Actualización, falta 1 ejercicio por realizar

--- MenuFiguras/app/src/main/java/com/example/menufiguras/MainActivity.java
+++ MenuFiguras/app/src/main/java/com/example/menufiguras/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.menufiguras;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Point;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,9 +16,12 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import static android.graphics.Bitmap.Config.ARGB_8888;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -26,6 +31,30 @@ public class MainActivity extends AppCompatActivity {
         registerForContextMenu(findViewById(R.id.helpButton));
 
     }
+    //Metodos del menú contextual
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_contextual, menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.CtxLblOpc1:
+                Toast.makeText(this, "Calcula el  area de la figura pasandole los datos necesarios", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.CtxLblOpc2:
+                Toast.makeText(this, "Dibuja la figura", Toast.LENGTH_LONG).show();
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
+
+    //Metodos del menú desplegable
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_principal, menu);
@@ -35,7 +64,13 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         ViewGroup viewGroup = findViewById(R.id.viewGroup);
         viewGroup.removeAllViews();
+        ImageView imageView;
+        Bitmap bitmap;
+        final Canvas canvas;
+        final Paint pincel;
+        CheckBox checkBoxGray, checkBoxGreen,checkBoxYellow;
         switch (item.getItemId()) {
+            //Botones Calcula
             case  R.id.MnuOpc1Calcula:
                 getLayoutInflater().inflate(R.layout.objects_area_rectangle,viewGroup);
                 Button area_rectangle_calculate=findViewById(R.id.edB1R);
@@ -77,7 +112,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case  R.id.MnuOpc4Calcula:
                 getLayoutInflater().inflate(R.layout.objects_area_square,viewGroup);
-                Button area_square_calculate=findViewById(R.id.edB1T);
+                Button area_square_calculate=findViewById(R.id.edB1S);
                 area_square_calculate.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
@@ -87,46 +122,253 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
                 break;
+            //Botones Dibuja
             case  R.id.MnuOpc1Dibuja:
-                getLayoutInflater().inflate(R.layout.objects_area_square,viewGroup);
-                Button area_rect_calculate=findViewById(R.id.edB1T);
-                area_square_calculate.setOnClickListener(new View.OnClickListener() {
+                //Inflamos el activity_main con el xml donde se ha definido el ImageView en el cual dibujaremos
+                getLayoutInflater().inflate(R.layout.lienzo,viewGroup);
+                imageView=findViewById(R.id.drawHere);
+                bitmap=Bitmap.createBitmap(200,200,ARGB_8888);
+                canvas=new Canvas(bitmap);
+
+                imageView.setImageBitmap(bitmap);
+                canvas.drawColor(Color.RED);
+
+                pincel=new Paint();
+                pincel.setColor(Color.BLUE);
+                pincel.setStrokeWidth(15);
+                pincel.setStyle(Paint.Style.STROKE);
+                canvas.drawRect(50,75,150,125,pincel);
+                checkBoxGray=findViewById(R.id.checkboxGray);
+                checkBoxGray.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        EditText edT1=findViewById(R.id.edT1S);
-                        TextView resultado=findViewById(R.id.etV1S_result);
-                        resultado.setText(String.valueOf(Math.pow(Float.parseFloat(edT1.getText().toString()),2)));
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.GRAY);
+                            canvas.drawRect(50,75,150,125,pincel);
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawRect(50,75,150,125,pincel);
+
+                        }
                     }
                 });
+                checkBoxGreen=findViewById(R.id.checkboxGreen);
+                checkBoxGreen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.GREEN);
+                            canvas.drawRect(50,75,150,125,pincel);
+
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawRect(50,75,150,125,pincel);
+
+                        }
+                    }
+                });
+                checkBoxYellow=findViewById(R.id.checkboxYellow);
+                checkBoxYellow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.YELLOW);
+                            canvas.drawRect(50,75,150,125,pincel);
+
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawRect(50,75,150,125,pincel);
+
+                        }
+                    }
+                });
+
                 break;
             case  R.id.MnuOpc2Dibuja:
-                getLayoutInflater().inflate(R.layout.draw_bitmap,viewGroup);
-                BitmapDrawable draw;
-                draw.
-                logo=(BitmapDrawable)res.getDrawable(R.drawable.pizzadepepperoni2);
-                logo.setBounds(new Rect(30,50,200,200));
-                break;
-            case  R.id.MnuOpc3Dibuja:
-                getLayoutInflater().inflate(R.layout.objects_area_square,viewGroup);
-                Button area_square_calculate=findViewById(R.id.edB1T);
-                area_square_calculate.setOnClickListener(new View.OnClickListener() {
+                getLayoutInflater().inflate(R.layout.lienzo,viewGroup);
+                imageView=findViewById(R.id.drawHere);
+                bitmap=Bitmap.createBitmap(200,200,ARGB_8888);
+                canvas=new Canvas(bitmap);
+
+                imageView.setImageBitmap(bitmap);
+                canvas.drawColor(Color.RED);
+
+                pincel=new Paint();
+                pincel.setColor(Color.BLUE);
+                pincel.setStrokeWidth(15);
+                pincel.setStyle(Paint.Style.STROKE);
+                canvas.drawCircle(100,100,90,pincel);
+                checkBoxGray=findViewById(R.id.checkboxGray);
+                checkBoxGray.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        EditText edT1=findViewById(R.id.edT1S);
-                        TextView resultado=findViewById(R.id.etV1S_result);
-                        resultado.setText(String.valueOf(Math.pow(Float.parseFloat(edT1.getText().toString()),2)));
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.GRAY);
+                            canvas.drawCircle(100,100,90,pincel);
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawCircle(100,100,90,pincel);
+
+                        }
+                    }
+                });
+                checkBoxGreen=findViewById(R.id.checkboxGreen);
+                checkBoxGreen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.GREEN);
+                            canvas.drawCircle(100,100,90,pincel);
+
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawCircle(100,100,90,pincel);
+
+                        }
+                    }
+                });
+                checkBoxYellow=findViewById(R.id.checkboxYellow);
+                checkBoxYellow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.YELLOW);
+                            canvas.drawCircle(100,100,90,pincel);
+
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawCircle(100,100,90,pincel);
+
+                        }
+                    }
+                });
+                break;
+
+            case  R.id.MnuOpc3Dibuja:
+                getLayoutInflater().inflate(R.layout.lienzo,viewGroup);
+                imageView=findViewById(R.id.drawHere);
+                bitmap=Bitmap.createBitmap(200,200,ARGB_8888);
+                canvas=new Canvas(bitmap);
+
+                imageView.setImageBitmap(bitmap);
+                canvas.drawColor(Color.RED);
+
+                pincel=new Paint();
+                pincel.setColor(Color.BLUE);
+                pincel.setStrokeWidth(10);
+                pincel.setStyle(Paint.Style.STROKE);
+
+                Point point1_draw = new Point(100, 20);
+                Point point2_draw = new Point(20, 170);
+                Point point3_draw = new Point(170, 170);
+
+                final Path path = new Path();
+                path.moveTo(point1_draw.x, point1_draw.y);
+                path.lineTo(point2_draw.x, point2_draw.y);
+                path.lineTo(point3_draw.x, point3_draw.y);
+                path.lineTo(point1_draw.x, point1_draw.y);
+                path.close();
+                canvas.drawPath(path, pincel);
+                checkBoxGray=findViewById(R.id.checkboxGray);
+                checkBoxGray.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.GRAY);
+                            canvas.drawPath(path, pincel);
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawPath(path, pincel);
+
+                        }
+                    }
+                });
+                checkBoxGreen=findViewById(R.id.checkboxGreen);
+                checkBoxGreen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.GREEN);
+                            canvas.drawPath(path, pincel);
+
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawPath(path, pincel);
+
+                        }
+                    }
+                });
+                checkBoxYellow=findViewById(R.id.checkboxYellow);
+                checkBoxYellow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.YELLOW);
+                            canvas.drawPath(path, pincel);
+
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawPath(path, pincel);
+
+                        }
                     }
                 });
                 break;
             case  R.id.MnuOpc4Dibuja:
-                getLayoutInflater().inflate(R.layout.objects_area_square,viewGroup);
-                Button area_square_calculate=findViewById(R.id.edB1T);
-                area_square_calculate.setOnClickListener(new View.OnClickListener() {
+                getLayoutInflater().inflate(R.layout.lienzo,viewGroup);
+                imageView=findViewById(R.id.drawHere);
+                bitmap=Bitmap.createBitmap(200,200,ARGB_8888);
+                canvas=new Canvas(bitmap);
+
+                imageView.setImageBitmap(bitmap);
+                canvas.drawColor(Color.RED);
+
+                pincel=new Paint();
+                pincel.setColor(Color.BLUE);
+                pincel.setStrokeWidth(15);
+                pincel.setStyle(Paint.Style.STROKE);
+                canvas.drawRect(50,50,150,150,pincel);
+                checkBoxGray=findViewById(R.id.checkboxGray);
+                checkBoxGray.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        EditText edT1=findViewById(R.id.edT1S);
-                        TextView resultado=findViewById(R.id.etV1S_result);
-                        resultado.setText(String.valueOf(Math.pow(Float.parseFloat(edT1.getText().toString()),2)));
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.GRAY);
+                            canvas.drawRect(50,50,150,150,pincel);
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawRect(50,50,150,150,pincel);
+
+                        }
+                    }
+                });
+                checkBoxGreen=findViewById(R.id.checkboxGreen);
+                checkBoxGreen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.GREEN);
+                            canvas.drawRect(50,50,150,150,pincel);
+
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawRect(50,50,150,150,pincel);
+
+                        }
+                    }
+                });
+                checkBoxYellow=findViewById(R.id.checkboxYellow);
+                checkBoxYellow.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if ( ((CheckBox)v).isChecked() ) {
+                            canvas.drawColor(Color.YELLOW);
+                            canvas.drawRect(50,50,150,150,pincel);
+
+                        }else{
+                            canvas.drawColor(Color.RED);
+                            canvas.drawRect(50,50,150,150,pincel);
+                        }
                     }
                 });
                 break;
@@ -136,27 +378,9 @@ public class MainActivity extends AppCompatActivity {
         }
         return true;
     }
-   @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo)
-    {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_contextual, menu);
-    }
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.CtxLblOpc1:
-                Toast.makeText(this, "Calcula el  area de la figura pasandole los datos necesarios", Toast.LENGTH_LONG).show();
-                return true;
-            case R.id.CtxLblOpc2:
-                Toast.makeText(this, "Dibuja la figura", Toast.LENGTH_LONG).show();
-                return true;
 
-            default:
-                return super.onContextItemSelected(item);
-        }
-    }
 
 }
+
+
 
